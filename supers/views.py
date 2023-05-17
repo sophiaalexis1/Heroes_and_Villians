92391505bf333f0988ff594ed3a48626eb213e3c
sophiaalexis1 <122618542+sophiaalexis1@users.noreply.github.com>
2023-05-17 12:59:18 -0400

    created urls path + tested path with 'ok' response

--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+
+@api_view(['GET', 'POST'])
+def supers_list(request):
+    
+    return Response ('ok')
+    
