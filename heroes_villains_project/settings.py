8355a162a1fde7e5e2a7225f0aa370ff6f09a6b6
sophiaalexis1 <122618542+sophiaalexis1@users.noreply.github.com>
2023-05-16 23:00:56 -0400

    added try & except statement in settings + created super_types and supers app + created urls.py and serializers.py in both applications

--- heroes_villains_project/settings.py
+++ heroes_villains_project/settings.py
@@ -107,3 +107,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from heroes_villains_project.local_settings import *
+except ImportError:
+    pass
