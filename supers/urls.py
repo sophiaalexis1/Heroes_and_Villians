0d4aa8dd46cbe364fd7f6b7e515a6da44ae5e6b0
sophiaalexis1 <122618542+sophiaalexis1@users.noreply.github.com>
2023-05-18 22:44:21 -0400

    created PUT coommand + craeted new path in urls.py

--- supers/urls.py
+++ supers/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.supers_list)
+    path('', views.supers_list),
+    path('<int:pk>/', views.supers_detail)
 ]
--- supers/views.py
+++ supers/views.py
@@ -27,4 +27,17 @@ def supers_list(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     
-    
+@api_view(['GET', 'PUT', 'DELETE'])
+def supers_detail(request, pk):
+     supers = get_object_or_404(Super, pk=pk)
+     if request.method == 'GET':
+         serializer = SuperSerializer(supers)
+         return Response(serializer.data)
+     elif request.method == 'PUT':
+         serializer = SuperSerializer(supers, data=request.data)
+         serializer.is_valid(raise_exception=True)
+         serializer.save()
+         return Response(serializer.data)
+     elif request.method == 'DELETE':
+         supers.delete()
+         return Response(status=status.HTTP_204_NO_CONTENT)
